6142b4a296257c0168e8c09eaec0fe11002d6cff
Vladimir <v.zhuravov@ospos.ru>
2018-02-17 09:25:26 +0300

    Удалил неактуальные Excel файлы

--- sql/Views/dv_sr_lspv_v.sql
+++ sql/Views/dv_sr_lspv_v.sql
@@ -3,14 +3,6 @@ create or replace view dv_sr_lspv_v as
          d.nom_ips,
          d.shifr_schet,
          d.data_op,
-         /*case
-           when sysdate <= to_date(20180331, 'yyyymmdd')
-                and d.ssylka_doc = 420328 and d.shifr_schet = 85 then
-             case
-               when d.nom_vkl = 5 and d.nom_ips = 10223 then -728
-               else d.summa
-             end
-           else d.summa--*/
          d.summa summa,
          d.ssylka_doc,
          d.kod_oper,
